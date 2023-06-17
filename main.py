722cbe80239af92700d87d53b99cab9c92e31983
Mike Pham <117026198+mike-anh-pham@users.noreply.github.com>
2023-06-17 15:54:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ st.write("This is a sample application.")
 
 alarm_clock = st.slider('hour', 0, 23, 17) # min: 0h, max: 23h, default: 17h
 st.header(alarm_clock) # print in large text
+
+
+from langchain.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader("example_data/layout-parser-paper.pdf")
+pages = loader.load_and_split()
+document = pages[0]
