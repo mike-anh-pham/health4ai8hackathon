f4259759c373d5c86547e9d296390bc6d1c4f626
Mike Pham <117026198+mike-anh-pham@users.noreply.github.com>
2023-06-17 16:25:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import streamlit as st
+from langchain.llms import OpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chains import ConversationalRetrievalChain
 
 st.title("Welcome to Health Universe!")
 st.write("This is a sample application.")
 
 alarm_clock = st.slider('hour', 0, 23, 17) # min: 0h, max: 23h, default: 17h
 st.header(alarm_clock) # print in large text
+
+# Inputs
+## Text input (not sidebar)
+openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 
 
 from langchain.document_loaders import PyPDFLoader
